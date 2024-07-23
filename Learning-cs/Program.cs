816c566d4b596bd3fbb570c4b751208dfbe611a9
Osama <iosamah187i@gmail.com>
2024-07-24 00:26:08 +0300

    Add project files.

---
+++ Learning-cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Learningcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beep();
+            PrintConsole("Hello World!");
+        }
+        static void Beep()
+        {
+            Console.Beep();
+        }
+        static void PrintConsole(string s)
+        {
+            Console.WriteLine(s);
+        }
+        static void Variables()
+        {
+            String firstName = "Osama";
+            String lastName = "Siran";
+            String fullName = firstName + lastName;
+            int age = 19;
+            double height = 5.7;
+            char symbol = 'M';
+            bool isOk = true;
+        }
+        static void Constants()
+        {
+            const String nationalId = "123456789";
+        }
+        static void TypeCasting()
+        {
+            // to"data type": String, Int32, Boolean, Char, Double.
+
+
+            String ageInString = "19";
+            int stringToInt = Convert.ToInt32(ageInString);
+
+            int idInInt = 123456789;
+            String intToString = Convert.ToString(idInInt);
+        }
+    }
+}
